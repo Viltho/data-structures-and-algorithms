a8e2f5e2d6411c0377a878f54ccebbf738f0c445
viltho <abdullahabuhammed@gmail.com>
2023-05-09 21:04:04 +0300

    AnimalShelter

---
+++ CodeChallenge12/test_tests12.py
@@ -0,0 +1,53 @@
+import pytest
+
+from CodeChallenge12.animalshelter import AnimalShelter, Dog, Cat
+
+def test_animal_shelter_enqueue_cat():
+    pq = AnimalShelter()
+    cat1 = Cat("Fluffy")
+    cat2 = Cat("Mittens")
+    pq.enqueue(cat1)
+    pq.enqueue(cat2)
+    expected = "[('Fluffy', 0), ('Mittens', 1)]"
+    actual = str(pq.cats)
+    assert actual == expected
+
+def test_animal_shelter_enqueue_dog():
+    pq = AnimalShelter()
+    cat1 = Cat("Fluffy")
+    cat2 = Cat("Mittens")
+    pq.enqueue(cat1)
+    pq.enqueue(cat2)
+    expected = "Fluffy"
+    actual = pq.dequeue("cat")
+    assert actual == expected
+
+def test_animal_shelter_dequeue_cat():
+    pq = AnimalShelter()
+    dog1 = Dog("Buddy")
+    dog2 = Dog("The Big Black")
+    pq.enqueue(dog1)
+    pq.enqueue(dog2)
+    expected = "Buddy"
+    actual = pq.dequeue("dog")
+    assert actual == expected
+
+def test_animal_shelter_dequeue_dog():
+    pq = AnimalShelter()
+    dog1 = Dog("Buddy")
+    dog2 = Dog("The Big Black")
+    pq.enqueue(dog1)
+    pq.enqueue(dog2)
+    expected = "Buddy"
+    actual = pq.dequeue("dog")
+    assert actual == expected
+
+def test_animal_shelter_dequeue_any():
+    pq = AnimalShelter()
+    dog1 = Dog("Buddy")
+    dog2 = Dog("The Big Black")
+    pq.enqueue(dog1)
+    pq.enqueue(dog2)
+    expected = None
+    actual = pq.dequeue("any")
+    assert actual == expected
--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 | Code Challenge 08    | [CC08](./CodeChallenge08/cc08.md)      |
 | Code Challenge 10    | [CC10](./CodeChallenge10/cc10.md)      |
 | Code Challenge 11    | [CC11](./CodeChallenge11/cc11.md)      |
+| Code Challenge 12    | [CC12](./CodeChallenge12/cc12.md)      |
 
 <!-- | Code Challenge 04    | [CC04](./CodeChallenge04/cc04.md)      |
 | Code Challenge 05    | [CC05](./CodeChallenge05/cc05.md)      |
