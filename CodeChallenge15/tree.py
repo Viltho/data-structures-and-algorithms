3b1c8c18ae67897d51903e2a8f958dd892ea8cb4
viltho <abdullahabuhammed@gmail.com>
2023-05-20 13:40:02 +0300

    updates on docstring

--- CodeChallenge15/tree.py
+++ CodeChallenge15/tree.py
@@ -12,22 +12,52 @@ class TreeNode:
     #     if root is not None:
     #         print(root.value), self.preorder(root.left), self.preorder(root.right)
     def preorder(self, root):
+        """the preorder function is to arrange the values in the given tree at the given position in the tree based on root first then left then right
+
+        Args:
+            root (node): node is the root of the tree
+
+        Returns:
+            list: a list of preorder traversed nodes.
+        """
         return [root.value] + self.preorder(root.left) + self.preorder(root.right) if root else []
             
     # def inorder(self, root):
     #     if root is not None:
     #         self.inorder(root.left), print(root.value), self.inorder(root.right)
     def inorder(self, root):
+        """the inorder function is to arrange the values in the given tree at the given position in the tree based on left first then root then right
+
+        Args:
+            root (node): node is the root of the tree
+
+        Returns:
+            list: a list of inorder traversed nodes.
+        """
         return self.inorder(root.left) + [root.value] + self.inorder(root.right) if root else []
         
     # def postorder(self, root):
     #     if root is not None:
     #         self.postorder(root.left), self.postorder(root.right), print(root.value)
     def postorder(self, root):
+        """the postorder function is to arrange the values in the given tree at the given position in the tree based on left first then right then root
+
+        Args:
+            root (node): node is the root of the tree
+
+        Returns:
+            list: a list of postorder traversed nodes.
+        """
         return self.postorder(root.left) + self.postorder(root.right) +  [root.value] if root else []
             
 class BST(TreeNode):
     def Add(self, root, value):
+        """adds a new node to the tree
+
+        Args:
+            root (node): base node reference to where the new node will be inserted
+            value (number): is the value of the node
+        """
         if root is None:
             self.root = Node(value)
         else:
@@ -43,6 +73,15 @@ class BST(TreeNode):
                     self.Add(root.left, value)
     
     def contains(self, node, value):
+        """search for a value in the tree
+
+        Args:
+            node (node): base node reference to where the search is performed
+            value (number): is the value of the search
+
+        Returns:
+            Boolean: returns true if tree contains value, and false otherwise
+        """
         if node is None:
             return False
         elif value == node.value:
